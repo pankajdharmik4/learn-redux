e481e8efb3d713dbabb6fe296e73777fa8b4058a
Pankaj Dharmik <pankajdharmik4@gmail.com>
2022-10-07 17:51:12 +0530

    Async Actions and Middleware used-thunk,logger

---
+++ asyncActions.js
@@ -0,0 +1,77 @@
+const redux = require('redux')
+const createStore = redux.createStore
+const applyMiddleware = redux.applyMiddleware
+const thunkMiddleware = require('redux-thunk').default
+const axios = require('axios')
+
+const initialState = {
+    loading:false,
+    users:[],
+    error:''
+}
+
+const FETCH_USER_REQUEST = 'FETCH_USER_REQUEST'
+const FETCH_USER_SUCCESS = 'FETCH_USER_SUCCESS'
+const FETCH_USER_FAILURE = 'FETCH_USER_FAILURE'
+
+const fetchUsersRequest=() =>{
+    return{
+        type:FETCH_USER_REQUEST
+    }
+}
+
+const fetchUsersSuccess=(users) =>{
+    return{
+        type:FETCH_USER_SUCCESS,
+        payload:users
+    }
+}
+
+const fetchUsersFailure=(error) =>{
+    return{
+        type:FETCH_USER_FAILURE,
+        payload: error
+    }
+}
+
+const reducer = (state=initialState,action) =>{
+    switch(action.type){
+        case FETCH_USER_REQUEST:
+            return{
+                ...state,
+                loading:true
+            }
+        case FETCH_USER_SUCCESS:
+            return{
+                ...state,
+                loading:false,
+                users: action.payload,
+                error: ''
+            }
+        case FETCH_USER_REQUEST:
+            return{
+                ...state,
+                loading:false,
+                users:[],
+                error: action.payload
+            }
+    }
+}
+
+const fetchUsers= () =>{
+    //JSONPLACEHOLDER
+    return function(dispatch){
+        dispatch(fetchUsersRequest)
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(response=>{
+                const users = response.data.map(user => user.id)
+                dispatch(fetchUsersSuccess(users))
+            })
+            .catch(error=>{
+                dispatch(fetchUsersFailure(error.message))
+            })
+    }
+}
+const store = createStore(reducer,applyMiddleware(thunkMiddleware))
+store.subscribe(()=>{console.log(store.getState())})
+store.dispatch(fetchUsers())
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const redux = require('redux')
+const reduxLogger = require('redux-logger')
+
+
 const combineReducers = redux.combineReducers
 const createStore = redux.createStore
+
+const applyMiddleware  = redux.applyMiddleware
+const logger = reduxLogger.createLogger()
 
 const BUY_CAKE = 'BUY_CAKE';
 const BUY_ICECREAM = 'BUY_ICECREAM'
@@ -73,9 +79,9 @@ const rootReducer = combineReducers({
     cake:cakeReducer,
     iceCream:IcecreamReducer
 })
-const store = createStore(rootReducer)
+const store = createStore(rootReducer,applyMiddleware(logger))
 console.log('Initital state', store.getState())
-const unsubscribe = store.subscribe(()=>console.log('Updated State', store.getState()))
+const unsubscribe = store.subscribe(()=>{})
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
