04147276bccbcc8bc36b4fabde7e2b03737a5270
Pankaj Dharmik <pankajdharmik4@gmail.com>
2022-10-07 23:02:05 +0530

    ownProps added

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -4,14 +4,19 @@ import {Provider} from 'react-redux';
 import store from './redux/store';
 import HooksCakeContainer from './Components/HooksCakeContainer';
 import IceCreamContainer from './Components/IceCreamContainer';
+import NewCakeContainer from './Components/NewCakeContainer';
+import ItemContainer from './Components/ItemContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake/>
+        <ItemContainer/>
         <CakeContainer/>
         <HooksCakeContainer/>
         <IceCreamContainer/>
+        <NewCakeContainer/>
       </div>
     </Provider>
 
