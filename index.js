0f954a8259c3a0c799c18bd8fc1fd62d0e155040
Pankaj Dharmik <pankajdharmik4@gmail.com>
2022-10-07 17:01:31 +0530

    Multiple reducers and How to combine them

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const redux = require('redux')
+const combineReducers = redux.combineReducers
 const createStore = redux.createStore
 
 const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 function buyCake(){
     return{
@@ -10,13 +12,43 @@ function buyCake(){
     }
 }
 
+function buyIcecream(){
+    return{
+        type:BUY_ICECREAM,
+        info: 'First redux action'
+    }
+}
+
 //(previousState, action) => newState
 
-const initialState= {
+// const initialState= {
+//     numOfCakes:10,
+//     numofIceCreams:20
+// }
+
+const initialCakeState={
     numOfCakes:10
 }
 
-const reducer =(state = initialState, action) =>{
+const initialIcecreamState = {
+    numofIceCreams:20
+}
+
+// const reducer =(state = initialState, action) =>{
+//     switch(action.type){
+//         case BUY_CAKE:return{ 
+//             ...state,
+//             numOfCakes: state.numOfCakes -1
+//         }
+//         case BUY_ICECREAM:return{ 
+//             ...state,
+//             numofIceCreams: state.numofIceCreams -1
+//         }
+//         default: return state
+//     }
+// }
+
+const cakeReducer =(state = initialCakeState, action) =>{
     switch(action.type){
         case BUY_CAKE:return{ 
             ...state,
@@ -26,12 +58,28 @@ const reducer =(state = initialState, action) =>{
     }
 }
 
-const store = createStore(reducer)
+const IcecreamReducer =(state = initialIcecreamState, action) =>{
+    switch(action.type){
+        case BUY_ICECREAM:return{ 
+            ...state,
+            numofIceCreams: state.numofIceCreams -1
+        }
+        default: return state
+    }
+}
+
+
+const rootReducer = combineReducers({
+    cake:cakeReducer,
+    iceCream:IcecreamReducer
+})
+const store = createStore(rootReducer)
 console.log('Initital state', store.getState())
 const unsubscribe = store.subscribe(()=>console.log('Updated State', store.getState()))
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIcecream())
+store.dispatch(buyIcecream())
 
 unsubscribe()
-
